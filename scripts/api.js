1784114fadefbef1d7f625be4e9304d3068bde70
Akim Desire <adesi006.ng@gmail.com>
2018-06-22 17:27:06 -0400

    first commit

---
+++ scripts/api.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const api = (function(){
+    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/akim'
+
+    //This func will get objects from the API
+    const getObjFromAPI = callback => {
+        $.getJSON(`${BASE_URL}/bookmarks`, callback);
+    }
+
+    //This Func will create items to post
+    const createBookmark2Post = function(info, callback){
+        const preppedInfo= JSON.stringify(info);
+        $.ajax({
+            url:`${BASE_URL}/bookmarks`,
+            method: 'POST',
+            contentType: 'application/json',
+            data: preppedInfo,
+            success: callback
+        });
+    } 
+    
+    
+    
+    
+    
+    
+    
+    
+    return {getObjFromAPI, createBookmark2Post}
+}())
